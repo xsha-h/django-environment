c27efbd8db0c48c2f462837834de250f48767840
hxs599663430 <599663430@qq.com>
2019-11-06 18:27:58 +0800

    添加日志函数封装

--- addlogs.py
+++ addlogs.py
@@ -5,5 +5,5 @@ from logs.models import Logs
 
 def addLog(user_id=None, log_module=None, scene=None, content=None):
     addtime = datetime.now()
-    log = Logs(user_id=user_id, log_module=log_module, scene=scene, content=content, addtime=addtime)
+    log = Logs(username=user_id, log_module=log_module, scene=scene, content=content, addtime=addtime)
     log.save()
